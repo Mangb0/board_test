ef5d5ed40e6313a4ad24558865adfe8652622557
Mangbo <whdudghks45629@gmail.com>
2022-11-13 19:03:48 +0900

    get Board List data
    get Board List data from postgresql
    /board/list

--- src/main/java/com/study/board/controller/BoardCotroller.java
+++ src/main/java/com/study/board/controller/BoardCotroller.java
@@ -4,6 +4,7 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,8 +30,9 @@ public class BoardCotroller {
     }
 
     @GetMapping("/board/list")
-    public String boardList() {
+    public String boardList(Model model) {
 
+        model.addAttribute("list", boardService.boardList());
         return "boardlist";
     }
 }
