b7e17ffdfd923fe0bc57f14b9f19c20117b3b6d6
Mangbo <whdudghks45629@gmail.com>
2022-11-15 23:26:52 +0900

    Make Modify Button
    게시글 상세페이지에 수정 버튼 추가(PathVariable 사용, 변경된 글 덮어씌워서 저장하여 처리)

--- src/main/java/com/study/board/controller/BoardCotroller.java
+++ src/main/java/com/study/board/controller/BoardCotroller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -41,6 +42,26 @@ public class BoardCotroller {
 
         model.addAttribute("board", boardService.boardView(boardno));
         return "boardview";
+    }
+
+    @GetMapping("/board/modify/{boardno}")
+    public String boardModify(@PathVariable("boardno") Integer boardno, Model model) {
+
+        model.addAttribute("board", boardService.boardView(boardno));
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{boardno}")
+    public String boardUpdate(@PathVariable("boardno") Integer boardno, Board board) {
+
+        Board boardTemp = boardService.boardView(boardno);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getTitle());
+        boardTemp.setWriter(board.getWriter());
+
+        boardService.write(boardTemp);
+
+        return "redirect:/board/list";
     }
 
     @GetMapping("/board/delete")
